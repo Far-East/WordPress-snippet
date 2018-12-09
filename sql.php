af6c330c82ccc511a8321159c0f15be3edd8507d
Егор <35236837+Far-East@users.noreply.github.com>
2018-12-09 13:49:41 +0300

    SQL-запросы, необходимые для переноса WordPress

---
+++ sql.php
@@ -0,0 +1,11 @@
+
+// SQL-запросы, необходимые для переноса сайта на WordPress на другой домен
+
+UPDATE wp_options SET option_value = REPLACE(option_value, 'http://site.ru', 'http://newsite') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_posts SET post_content = REPLACE (post_content, 'http://site.ru', 'https://newsite');
+UPDATE wp_postmeta SET meta_value = REPLACE (meta_value, 'http://site.ru','https://newsite');
+
+// Если переносим с localhost то в файл wp-config добавляем 
+
+define('WP_HOME','http://site.ru');
+define('WP_SITEURL','http://newsite.ru');
