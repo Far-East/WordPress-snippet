1b829c548d9f2755a0dd0678ec943fb5195c1d9a
Егор <35236837+Far-East@users.noreply.github.com>
2019-06-10 07:29:52 +0300

    RE-Modal

---
+++ remodal.php
@@ -0,0 +1,25 @@
+<?php
+
+// Подключаю
+
+wp_enqueue_style( 'modal', get_template_directory_uri() . '/remodal/remodal.css', array());
+
+wp_enqueue_script( 'modal', get_template_directory_uri() . '/remodal/remodal.min.js', array( 'jquery' ),
+	true );
+  
+ // Вызов 
+<a href="#modal">Call the modal with data-remodal-id="modal"</a>
+  
+ // Содержимое
+<div class="remodal" data-remodal-id="modal"
+  data-remodal-options="hashTracking: false, closeOnOutsideClick: false">
+
+  <button data-remodal-action="close" class="remodal-close"></button>
+  <h3>Remodal</h3>
+  <p>
+    Responsive, lightweight, fast, synchronized with CSS animations, fully customizable modal window plugin with declarative configuration and hash tracking.
+  </p>
+</div>
+  
+  
+  
