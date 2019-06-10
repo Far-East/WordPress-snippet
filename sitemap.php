54a59363985771ebd737c343bf6be4adb6a3f9b0
Егор <35236837+Far-East@users.noreply.github.com>
2019-06-10 11:44:58 +0300

    Карта сайта

--- sitemap.php
+++ sitemap.php
@@ -1,34 +1,28 @@
-							<!--Простая карта сайта===========================================-->
-							<h3>Страницы</h3>
-							<ul>
-								<?php wp_list_pages('title_li='); ?>
-							</ul>
-							
-							<h3>Публикации</h3>
-							<?php
-							// запрос
-							$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
-							
-							<?php if ( $wpb_all_query->have_posts() ) : ?>
+<!--Простая карта сайта===========================================-->
+<h3>Страницы</h3>
+<ul>
+	<?php wp_list_pages('title_li='); ?>
+</ul>
 
-								<ul>
+<h3>Публикации</h3>
+<?php
+// запрос
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
 
-									<!-- the loop -->
-									<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-										<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-									<?php endwhile; ?>
-									<!-- end of the loop -->
+<?php if ( $wpb_all_query->have_posts() ) : ?>
 
-								</ul>
-								
-								<?php wp_reset_postdata(); ?>
-							
-							<?php else : ?>
-								<p><?php _e( 'Извините, нет записей, соответствуюших Вашему запросу.' ); ?></p>
-							<?php endif; ?>
+	<ul>
 
+		<!-- the loop -->
+		<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+		<?php endwhile; ?>
+		<!-- end of the loop -->
 
+	</ul>
+	
+	<?php wp_reset_postdata(); ?>
 
-
-
-
+<?php else : ?>
+	<p><?php _e( 'Извините, нет записей, соответствуюших Вашему запросу.' ); ?></p>
+<?php endif; ?>
