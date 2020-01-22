b2b76aaa1c149440f5fa460834e32a289b53808f
Егор <35236837+Far-East@users.noreply.github.com>
2020-01-22 17:40:27 +0300

    Update template-functions.php

--- template-functions.php
+++ template-functions.php
@@ -194,7 +194,18 @@ if ( ! function_exists( 'fw_showimagesizes' ) ) {
 	}
 	add_filter('image_size_names_choose', 'fw_showimagesizes');
 }
+/**
+ * Заполняет поле для атрибута alt на основе заголовка Записи у картинки при её добавлении в контент.
+ */
+function change_empty_alt_to_title( $response ) {
+	if ( ! $response['alt'] ) {
+		$response['alt'] = sanitize_text_field( $response['uploadedToTitle'] );
+	}
+	
+	return $response;
+}
 
+add_filter( 'wp_prepare_attachment_for_js', 'change_empty_alt_to_title' );
 /**
  * Отключает вкладку комментарии в админпанели
  */
