72d64a529bbc3f8666d903f4ffdd42f601e6fd16
Егор <35236837+Far-East@users.noreply.github.com>
2020-01-24 15:09:04 +0300

    Авто-обновления

---
+++ auto-update.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Авто-обновления
+ * functions.php
+ */
+ 
+ // вкл. обновл. ядра
+add_filter( 'auto_update_core', '__return_true' );
+
+// вкл. обновл. всех тем
+add_filter( 'auto_update_theme', '__return_true' );
+
+// вкл. обновл. всех  плагинов
+add_filter( 'auto_update_plugin', '__return_true' );
